0242b88f034b7f6a8a9d29917b2a29493d820fce
root <root@EST1004649.sms.fortaleza>
2024-10-29 15:58:08 -0300

    routes-controllers-views-migrations-seeders

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello World!";
-});
-
-Route::get('/about', function(){
-    echo "About Us";
-});
+// Auth routs
+Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::get('/logout', [AuthController::class, 'logout']);
