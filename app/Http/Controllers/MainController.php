a8320580b0878c1886f9cea67d6e356965ba70c9
root <root@EST1004649.sms.fortaleza>
2024-10-31 13:25:07 -0300

    model-middleware

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index()
+    {
+        // load user's notes
+
+        // show home view
+        return view('home');
+    }
+    public function newNote()
+    {
+        echo "I'm creating a new note!";
+    }
 }
